30df47a92672fa71f725154c275f6cb2e6302820
hajoon <stravinskky@naver.com>
2025-04-14 11:08:37 +0900

    모듈

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import styles from './IndexPage.module.scss';
+import Module from '../components/hajoon/Module';
 
 const IndexPage = () => {
   return (
     <>
-      <div className={styles.home}>
-        <h1 className={styles.title}>개발자의 기술 블로그</h1>
-        <p className={styles.subtitle}>React와 관련된 기술들을 공유합니다.</p>
-      </div>
+      <Module />
     </>
   );
 };
