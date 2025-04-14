35557e697a44c10710b0314cd8943335aead7101
dojinho <20191841@kiu.kr>
2025-04-14 18:19:54 +0900

    병합처리

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -3,6 +3,7 @@ import styles from './RootLayout.module.scss';
 import MainNav from '../components/MainNav';
 import { Outlet } from 'react-router-dom';
 
+
 const RootLayout = () => {
   return (
     <div className={styles.layout}>
