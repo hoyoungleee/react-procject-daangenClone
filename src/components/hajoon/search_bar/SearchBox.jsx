f132bd74acbe02a9cb81fac8b2c6cb4fcf3ba762
hajoon <stravinskky@naver.com>
2025-04-15 09:27:51 +0900

    검색상자 x버튼

--- src/components/hajoon/search_bar/SearchBox.jsx
+++ src/components/hajoon/search_bar/SearchBox.jsx
@@ -62,11 +62,23 @@ const SearchBox = () => {
 
         <div>
           <input
+            value={inputValue}
             placeholder='검색어를 입력하세요'
             onChange={(e) => {
               setInputValue(e.target.value);
             }}
           />
+
+          {inputValue && (
+            <button
+              className={styles.clearButton}
+              value={inputValue}
+              onClick={() => setInputValue('')}
+            >
+              x
+            </button>
+          )}
+
           <button className={styles.ArrowButton}>
             <svg
               viewBox='0 0 24 24'
