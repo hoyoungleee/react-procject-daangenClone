d74d644306141b7727a48e48ecd26fac872dda70
hajoon <stravinskky@naver.com>
2025-04-15 19:12:29 +0900

    Link 걸기(SearchParam, Link)

--- src/components/hajoon/Category.jsx
+++ src/components/hajoon/Category.jsx
@@ -6,13 +6,13 @@ const Category = () => {
   return (
     <div className={styles.category}>
       <ul>
-        <ImageList type='Bag' text='중고거래' />
-        <ImageList type='Work' text='알바' />
-        <ImageList type='House' text='부동산' />
-        <ImageList type='Car' text='중고차' />
-        <ImageList type='HomeTown' text='동네업체' />
-        <ImageList type='News' text='동네생활' />
-        <ImageList type='Crew' text='모임' />
+        <ImageList type='Bag' text='중고거래' link={'used-items'} />
+        <ImageList type='Work' text='알바' link={'work'} />
+        <ImageList type='House' text='부동산' link={'house'} />
+        <ImageList type='Car' text='중고차' link={'car'} />
+        <ImageList type='HomeTown' text='동네업체' link={'hometown'} />
+        <ImageList type='News' text='동네생활' link={'community'} />
+        <ImageList type='Crew' text='모임' link={'crew'} />
       </ul>
     </div>
   );
